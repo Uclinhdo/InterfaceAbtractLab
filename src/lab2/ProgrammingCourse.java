defa2868343d7ad71847a0ac0d687f8af0e7a40c
ldo <ldo@L11403043630.wctc.msft>
2016-09-19 15:53:18 -0500

    Add new methods and modify the old methods.

--- src/lab2/ProgrammingCourse.java
+++ src/lab2/ProgrammingCourse.java
@@ -5,10 +5,30 @@
  */
 package lab2;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author ldo
  */
-public class ProgrammingCourse {
+public interface ProgrammingCourse {
+    
+     public abstract String getCourseName();
+
+    public abstract void setCourseName(String courseName);
+    
+     public abstract String getCourseNumber();
+
+    public abstract void setCourseNumber(String courseNumber);
+    
+    public abstract double getCredits() ;
+
+    public abstract void setCredits(double credits);
+    
+    public abstract String getPrerequisites();
+
+    public abstract void setPrerequisites(String prerequisites);
+    
+    
     
 }
