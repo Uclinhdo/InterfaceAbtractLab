07f9a4fbbb1d06b79364aeafff7420091a66cae6
ldo <ldo@L11403043630.wctc.msft>
2016-09-19 15:53:11 -0500

    Add new methods and modify the old methods.

--- src/lab2/IntroToProgrammingCourse.java
+++ src/lab2/IntroToProgrammingCourse.java
@@ -8,54 +8,45 @@ import javax.swing.JOptionPane;
  * @author      your name goes here
  * @version     1.00
  */
-public class IntroToProgrammingCourse {
-    private String courseName;
-    private String courseNumber;
-    private double credits;
+public class IntroToProgrammingCourse implements ProgrammingCourse {
+   
 
     public IntroToProgrammingCourse(String courseName, String courseNumber) {
-        this.setCourseName(courseName);
-        this.setCourseNumber(courseNumber);
+        setCourseName(courseName);
+        setCourseNumber(courseNumber);
     }
 
-    public String getCourseNumber() {
-        return courseNumber;
-    }
-
-    public final void setCourseNumber(String courseNumber) {
-        if(courseNumber == null || courseNumber.length() == 0) {
-            JOptionPane.showMessageDialog(null,
-                    "Error: courseNumber cannot be null of empty string");
-            System.exit(0);
-        }
-        this.courseNumber = courseNumber;
-    }
-
-    public double getCredits() {
-        return credits;
-    }
-
-    public void setCredits(double credits) {
-        if(credits < 0.5 || credits > 4.0) {
-            JOptionPane.showMessageDialog(null,
-                    "Error: credits must be in the range 0.5 to 4.0");
-            System.exit(0);
-        }
-        this.credits = credits;
-    }
-
-    public String getCourseName() {
-        return courseName;
+    public final String getCourseName() {
+    return getCourseName();
     }
 
     public final void setCourseName(String courseName) {
-        if(courseName == null || courseName.length() == 0) {
-            JOptionPane.showMessageDialog(null,
-                    "Error: courseName cannot be null of empty string");
-            System.exit(0);
-        }
-        this.courseName = courseName;
+        
     }
 
+    public final String getCourseNumber() {
+       return getCourseNumber();
+    }
+
+    public final void setCourseNumber(String courseNumber) {
+        
+    }
+
+    public final double getCredits() {
+        return getCredits();
+    }
+
+    public final void setCredits(double credits) {
+      
+    }
+
+    public String getPrerequisites() {
+        return getPrerequisites();
+    }
+    public void setPrerequisites(String prerequisites) {
+        
+    }
+
+    
     
 }
